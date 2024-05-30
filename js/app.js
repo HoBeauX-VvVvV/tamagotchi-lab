a8696c3d25daf4401487129c326dcca4737f4bfc
BeauX <fire-fly_7@hotmail.com>
2024-05-30 14:26:50 -0700

    setup

---
+++ js/app.js
@@ -0,0 +1,40 @@
+/*-------------------------------- Constants --------------------------------*/
+
+// 1) Define the required variables used to track the state of the game.
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+// 1) Define the required variables used to track the state of the game.
+
+/*------------------------ Cached Element References ------------------------*/
+
+// 2) Store cached element references.
+
+/*-------------------------------- Functions --------------------------------*/
+
+// 3) Upon loading, the game state should be initialized, and a function should 
+//    be called to render this game state.
+
+// 4) The state of the game should be rendered to the user.
+
+// 5) Handle the game over logic.
+
+// 7) Create reset functionality.
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+// 6) Handle each instance of a player clicking a button with the use of a 
+//    `handleClick()` function.
+
+
+
+
+
+
+
+
+
+
+
+
+
